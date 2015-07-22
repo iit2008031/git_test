f637258b9c51fd6849834470985adbcd95a6b566
iit2008031 <ankit.itcool@gmail.com>
2015-07-22 14:37:37 +0530

    initial commit

--- testing.c
+++ testing.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 
+int fib(int n)
+{
+	if(n==0)
+		return 0;
+	else if(n==1)
+		return 1;
+	else 
+		return fib(n-1)+fib(n-2);	
+}
+
 int main()
 {
 	printf("Hello Worls");
+	printf("%d",fib(34));
 	return 0;
 }
